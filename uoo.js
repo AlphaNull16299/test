b3766d0c11c9f2458e1b74aa7343e7f1516294a6
AlphaNull <58626192+AlphaNull16299@users.noreply.github.com>
2021-12-23 11:03:18 +0900

    new math

--- uoo.js
+++ uoo.js
@@ -1,10 +1,28 @@
+let readlineSync = require("readline-sync");
 let math = require("mathjs");
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
-readline.question('input num ', (answer) => {
-    console.log(math.sqrt(answer));
-    readline.close();
-});
+let variables = {};
+let flagFinish = false;
+
+do{
+    process.stdout.write("\u001b[36m数式を入力\u001b[0m\n    = ");
+    let input = readlineSync.question();
+
+    if(input.startsWith(">$")){
+        let variableName = input.slice(1)
+        variables[variableName] = variables.$ans;
+        process.stdout.write(` \x1b[4m${variableName} に $ans (${variables.$ans}) が保存されました。\u001b[0m\n`);
+    }else if(input.startsWith(">exit")){
+        flagFinish = true;
+    }else{
+        let ans = math.evaluate(input, variables);
+
+        if(ans){
+            process.stdout.write(` ans: \x1b[4m\x1b[35m${ans}\u001b[0m\n`);
+            variables.$ans = ans;
+        }
+    }
+
+    process.stdout.write("\n");
+
+}while(!flagFinish);
